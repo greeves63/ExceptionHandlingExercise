ec878cbf9bdcab76e9dacc8765b11938f78e7758
Gregory Reeves Jr <gregoryreevesjr@gmail.com>
2020-07-13 15:02:51 -0500

    Task completed and updated

--- ExceptionHandlingExercise/Program.cs
+++ ExceptionHandlingExercise/Program.cs
@@ -9,21 +9,39 @@ namespace ExceptionHandlingExercise
         static void Main(string[] args)
         {
             // -----------------------------------------------------------------------------
-            // First create an char[], it must contain 6 numbers and 3 letters - name it arr
-            // Create a list called numbers that will hold integers
-            // Create an string variable with an empty string initializer - name it str
+            // DONE - First create an char[], it must contain 6 numbers and 3 letters - name it arr
+            // DONE - Create a list called numbers that will hold integers
+            // DONE - Create an string variable with an empty string initializer - name it str
 
             // using a foreach loop, attempt to parse the elements in your char[] with int.Parse()
             // and Exceptions will be thrown 
             // Below we will set this up 
             // ------------------------------------------------------------------------------
 
-
+            var arr = new char[] { '1', '2', '3', '4', '5', '6', 'a', 'b', 'c' };
+            var numbers = new List<int>();
+            var str = "";
 
             //TODO START HERE:
             
             // Make a foreach loop to iterate through your character array
-            
+            foreach  (var character in arr)
+            {
+                try
+                {
+                    str = character.ToString();
+                    int number = int.Parse(str);
+
+                    numbers.Add(number);
+
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Unable to Parse '{character}'"); //character will be the name of each item in your collection
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
                 // Now create a try catch
                 
                 
@@ -39,10 +57,11 @@ namespace ExceptionHandlingExercise
                 
             
 
-            //foreach (var num in numbers)
-            //{
-            //    Console.WriteLine(num);
-            //}
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
         }
     }
 }
